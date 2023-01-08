3b496e5eede3c1dfd05970ec901a5954d71aa962
IceBear9028 <tlatms9028@gmail.com>
2023-01-08 23:59:27 +0900

    17.1 commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import {BrowserRouter ,Routes, Route} from "react-router-dom";
+import {BrowserRouter ,Routes, Route, createBrowserRouter} from "react-router-dom";
 import {Login} from './components/page/LoginPage/Login'
 import {Main} from "./components/page/MainPage/Main";
 import {Register} from "./components/page/RegisterPage/Register";
+import ErrorPage from "./components/page/ErrorPage/Error";
 import Auth from "./hoc/auth";
 // react의 어나니머스 컴포넌트를 불러올 때, 자신이 원하는 이름으로 불러올 수 있다.
 
@@ -10,6 +11,7 @@ function App() {
     const AuthLogin = Auth(Login, true);
     const AuthRegister = Auth(Register, null);
     const AuthMain = Auth(Main, true);
+
 
     return (
         <>
@@ -19,6 +21,7 @@ function App() {
                         <Route path = "/login" element={<AuthLogin/>}/>
                         <Route path = "/main" element={<AuthMain/>}/>
                         <Route path = "/register" element={<AuthRegister/>}/>
+                        <Route path = '/error' element={<ErrorPage/>}/>
                     </Routes>
                 </div>
             </BrowserRouter>
